22b9d4427ca15abf23660acdfd1b7aa80f1cd8b8
Jorge Guedes <jorge_guedes00@hotmail.com>
2023-11-30 22:24:12 -0300

    feat: add CartItem component and improved cart product addition

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -2,12 +2,14 @@ import { ShoppingCartIcon } from "lucide-react";
 import { Badge } from "./badge";
 import { useContext } from "react";
 import { CartContext } from "@/providers/cart";
+import CartItem from "./cart-item";
+import { computeProductTotalPrice } from "@/helpers/products";
 
 const Cart = () => {
   const { products } = useContext(CartContext);
 
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <Badge
         className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
         variant="outline"
@@ -17,9 +19,16 @@ const Cart = () => {
       </Badge>
 
       {/* RENDERIZAR OS PRODUTOS */}
-      {products.map((product) => (
-        <h1 key={product.id}>{product.name}</h1>
-      ))}
+      <div className="flex flex-col gap-5">
+        {products.map((product) => (
+          <CartItem
+            product={computeProductTotalPrice(product as any) as any}
+            key={product.id}
+          >
+            {product.name}
+          </CartItem>
+        ))}
+      </div>
     </div>
   );
 };
