56cb80c3595f6c55024ee16f9d545d02deb07f30
Jorge Guedes <jorge_guedes00@hotmail.com>
2023-12-01 23:03:15 -0300

    feat: add stripe web hook

--- src/actions/checkout.ts
+++ src/actions/checkout.ts
@@ -5,7 +5,6 @@ import Stripe from "stripe";
 
 export const createCheckout = async (products: CartProduct[]) => {
   // CRIAR CHECKOUT
-
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
     apiVersion: "2023-10-16",
   });
